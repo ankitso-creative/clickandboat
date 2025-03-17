e017d6de8663e09432463d4bbbac881127dc521d
ankitso-creative <ankit@so-creative.co.uk>
2025-03-17 16:36:20 +0530

    Listing

--- database/migrations/2024_11_28_112305_create_prices_table.php
+++ database/migrations/2024_11_28_112305_create_prices_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('prices', function (Blueprint $table) {
             $table->id();
             $table->foreignId('listing_id')->constrained()->onDelete('cascade');
-            $table->foreignId('season_price_id')->constrained()->onDelete('cascade');
+            $table->foreignId('season_price_id')->constrained()->onDelete('cascade')->nullable();
             $table->string('price');
             $table->string('over_night_price')->nullable();
             $table->string('one_half_day')->nullable();
