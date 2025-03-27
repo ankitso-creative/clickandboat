11359a3a066d47a6713cb2e786ed9bc9b5f3fbdf
abhishekthakur98 <abhishek@so-creative.co.uk>
2025-03-27 11:10:59 +0530

    asdalklkls

--- resources/views/front/thankyou.blade.php
+++ resources/views/front/thankyou.blade.php
@@ -1,14 +1,34 @@
 @extends('layouts.front.common')
 
 @section('meta')
-    <title>Manage Users</title>
+<title>Manage Users</title>
 @endsection
 @section('css')
-    
+
 @endsection
 @section('js')
-    
+
 @endsection
 @section('content')
-   <p>add code here</p>
+<section class="thank_you_section">
+    <div class="thank_you_section_text">
+        <h1>Thank You</h1>
+    </div>
+</section>
+<section class="thanks_page_text">
+    <div class="container">
+        <div class="col-md-12">
+            <div class="text-center thank_section_text">
+                <h2>Thank You</h2>
+                <i class="fa-solid fa-check"></i>
+                <p>Een van ons team zal contact met u opnemen.
+                Of neem contact op via onderstaande gegevens.</p>
+                <a href="">Back to Home</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
 @endsection
