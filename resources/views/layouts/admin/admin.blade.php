7918c5af756c16ea6af61a0d8997e998138acec3
ankitso-creative <ankit@so-creative.co.uk>
2025-03-28 17:18:46 +0530

    new

--- resources/views/layouts/admin/admin.blade.php
+++ resources/views/layouts/admin/admin.blade.php
@@ -50,7 +50,7 @@
 
     <!-- BEGIN PAGE LEVEL PLUGINS -->
     <link href="{{ asset('app-assets/global/plugins/bootstrap-sweetalert/sweetalert.css') }}" rel="stylesheet"type="text/css" />
-    <link href="{{ asset('app-assets/global/css/style.css') }}" rel="stylesheet"type="text/css" />
+    <link href="{{ asset('app-assets/global/css/style.css') }}?ver=<?=time()?>" rel="stylesheet"type="text/css" />
     <!-- END PAGE LEVEL PLUGINS -->
     <link rel="shortcut icon" href="favicon.ico" />
     <script src="{{ asset('app-assets/global/plugins/jquery.min.js') }}" type="text/javascript"></script>
