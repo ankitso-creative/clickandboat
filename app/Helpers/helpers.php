a4d733d3150ed056b5f8588283deb61b0b4834d6
ankitso-creative <ankit@so-creative.co.uk>
2025-03-31 16:31:00 +0530

    new

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,81 +1,118 @@
 <?php
-    use App\Models\Admin\Setting;
-    use App\Models\Admin\Price;
-    use Carbon\Carbon;
-    function clean($string,$symbol="-") 
-    {
-        $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
-        $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
-        
-        return preg_replace('/-+/', $symbol, strtolower($string)); // Replaces multiple hyphens with single one.
-    }
-    function phoneWebsite()
-    {
-        $setting = Setting::where('meta_key','phone')->first();
-        $meta_value = $setting->meta_value;
-        return $meta_value;
-    }
-    function emailWebsite()
-    {
-        $setting = Setting::where('meta_key','email')->first();
-        $meta_value = $setting->meta_value;
-        return $meta_value;
-    }
-    function logoURL()
-    {
-        $setting = Setting::first();
-        $logo = $setting->getFirstMedia('logo');
-        return $logo->getUrl() ?? '';
-    }
-    function whiteLogoURL()
-    {
-        $setting = Setting::first();
-        $logo = $setting->getFirstMedia('logo-white');
-        return $logo->getUrl() ?? '';
-    }
-    function userName()
-    {
-        $user = Auth::user();
-        $userName = $user->name;
-        return $userName;
-    }
-    function userImage()
-    {
-        $user = Auth::user();
-        $image = $user->getFirstMediaUrl('profile_image');
-        if(!$image):
-            $image = 'https://static1.clickandboat.com/v1/o/img/mask~dddc60cc1d.png';
-        endif;
-        return $image;
-    }
-    function bookingPrice($request)
-    {
-        $price = Price::where('listing_id',$request['id'])->first();
-        $startDate = Carbon::parse($request['checkindate']);
-        $endDate = Carbon::parse($request['checkoutdate']);
-        $days = $startDate->diffInDays($endDate);
-        if($days >= 1)
-        {
-            if($price) 
-            {
+
+use App\Models\Admin\Setting;
+use App\Models\Admin\Price;
+use App\Models\Admin\SeasonPrice;
+use Carbon\Carbon;
+
+function clean($string, $symbol = "-")
+{
+    $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+    $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+
+    return preg_replace('/-+/', $symbol, strtolower($string)); // Replaces multiple hyphens with single one.
+}
+function phoneWebsite()
+{
+    $setting = Setting::where('meta_key', 'phone')->first();
+    $meta_value = $setting->meta_value;
+    return $meta_value;
+}
+function emailWebsite()
+{
+    $setting = Setting::where('meta_key', 'email')->first();
+    $meta_value = $setting->meta_value;
+    return $meta_value;
+}
+function logoURL()
+{
+    $setting = Setting::first();
+    $logo = $setting->getFirstMedia('logo');
+    return $logo->getUrl() ?? '';
+}
+function whiteLogoURL()
+{
+    $setting = Setting::first();
+    $logo = $setting->getFirstMedia('logo-white');
+    return $logo->getUrl() ?? '';
+}
+function userName()
+{
+    $user = Auth::user();
+    $userName = $user->name;
+    return $userName;
+}
+function userImage()
+{
+    $user = Auth::user();
+    $image = $user->getFirstMediaUrl('profile_image');
+    if (!$image):
+        $image = 'https://static1.clickandboat.com/v1/o/img/mask~dddc60cc1d.png';
+    endif;
+    return $image;
+}
+function bookingPrice($request)
+{
+    $startDate = Carbon::parse($request['checkindate']);
+    $endDate = Carbon::parse($request['checkoutdate']);
+    $days = $startDate->diffInDays($endDate);
+    $seasonData = SeasonPrice::where('listing_id', $request['id'])->get();
+    $hasError  = true;
+    if ($days >= 1) {
+        if (count($seasonData)) {
+            foreach ($seasonData as $key => $season) {
+                $from = Carbon::parse($season->from . ' 1st')->month;
+                $to = Carbon::parse($season->to)->month;
+                $month = $startDate->month;
+                if ($month >= $from && $month <= $to) {
+                    $hasError=false;
+                    $seasonId = $season->id;
+                    $price = Price::where('season_price_id', $seasonId)->first();
+                    if ($price) {
+                        $priceArray = $price->toArray();
+                        if ($priceArray['one_half_day'] && $days == 1):
+                            $total_price = $priceArray['one_half_day'];
+                        elseif ($priceArray['two_day'] && $days == 2):
+                            $total_price = $priceArray['two_day'];
+                        elseif ($priceArray['three_day'] && $days == 3):
+                            $total_price = $priceArray['three_day'];
+                        elseif ($priceArray['four_day'] && $days == 4):
+                            $total_price = $priceArray['four_day'];
+                        elseif ($priceArray['five_day'] && $days == 5):
+                            $total_price = $priceArray['five_day'];
+                        elseif ($priceArray['six_day'] && $days == 6):
+                            $total_price = $priceArray['six_day'];
+                        elseif ($priceArray['one_week'] && $days <= 7):
+                            $total_price = $priceArray['one_week'];
+                        else:
+                            $total_price = $season->price * $days;
+                        endif;
+                        $servive_fee = 0;
+                        $totalAmount = $total_price + $servive_fee;
+                        $result = [
+                            'price' => $total_price,
+                            'days' => $days,
+                            'servive_fee' => $servive_fee,
+                            'totalAmount' => $totalAmount,
+                        ];
+                    } else {
+                        $total_price = $season->price * $days;
+                        $servive_fee = 0;
+                        $totalAmount = $total_price + $servive_fee;
+                        $result = [
+                            'price' => $total_price,
+                            'days' => $days,
+                            'servive_fee' => $servive_fee,
+                            'totalAmount' => $totalAmount,
+                        ];
+                    }
+                }
+            }
+        } else {
+            $price = Price::where('listing_id', $request['id'])->first();
+            if ($price) {
                 $priceArray = $price->toArray();
-                if($priceArray['one_half_day'] && $days == 1):
-                    $total_price = $priceArray['one_half_day'] * $days;
-                elseif($priceArray['two_day'] && $days == 2):
-                    $total_price = $priceArray['two_day'] * $days;
-                elseif($priceArray['three_day'] && $days == 3):
-                    $total_price = $priceArray['three_day']  * $days;
-                elseif($priceArray['four_day'] && $days == 41):
-                    $total_price = $priceArray['four_day']  * $days;
-                elseif($priceArray['five_day'] && $days == 5):
-                    $total_price = $priceArray['five_day'] * $days;
-                elseif($priceArray['six_day'] && $days == 6):
-                    $total_price = $priceArray['six_day'] * $days;
-                elseif($priceArray['one_week'] && $days <= 7):
-                    $total_price = $priceArray['one_week'] * $days;
-                else:
-                    $total_price = $priceArray['price'] * $days;
-                endif;
+                $total_price = $priceArray['price'] * $days;
                 $servive_fee = 0;
                 $totalAmount = $total_price + $servive_fee;
                 $result = [
@@ -84,135 +121,127 @@
                     'servive_fee' => $servive_fee,
                     'totalAmount' => $totalAmount,
                 ];
-            } 
-            else 
-            {
+            } else {
+                $total_price = 0;
                 $result['error'] = 'Product not found';
             }
         }
-        else
+        if($hasError)
         {
-            $result['error'] = '';
-        }
-        return $result;
-    }
-    function selectOption($table, $columnName, $columnValue, $selectedValue='', $condition = '', $orderBy = '')
-    {
-        $html ="";
-
-        $items = DB::table($table)->select($columnValue, $columnName)
-        ->when(is_array($condition), function($query) use($condition){
-            $query->where($condition[0],$condition[1]);
-        })
-        ->when(is_array($orderBy), function($query) use($condition){
-            $query->orderBy($orderBy[0],$orderBy[1]);
-        })->get()->toArray();
-
-        if($items):
-            foreach($items as $item):
-                $item = (array) $item;
-                $selected = '';
-                if($item[$columnValue]==$selectedValue):
-                    $selected = 'selected';
-                endif;
-                $html .= '<option value = "'.$item[$columnValue].'" '. $selected.'>'.$item[$columnName].'</option>';
-            endforeach;
-        endif;
-        
-        return $html;
-    }
-    function checkCheckbox($array,$value)
-    {
-        if(!empty($array)):
-            if(in_array($value,$array)):
-                return 'checked';
-            else:
-                return false;
-            endif;
-        else:
-            return false;
-        endif;
-    }
-    function singleCheckbox($selected,$value)
-    {
-        if(!empty($selected)):
-            if($value==$selected):
-                return 'checked';
-            else:
-                return false;
-            endif;
-        else:
-            return false;
-        endif;
-    }
-    function checkradio($selected,$value)
-    {
-        if(!empty($selected)):
-            if($value==$selected):
-                return 'checked';
-            else:
-                return false;
-            endif;
-        else:
-            return false;
-        endif;
-    }
-    function checkselect($selected,$value)
-    {
-        if(!empty($selected)):
-            if($value==$selected):
-                return 'selected';
-            else:
-                return false;
-            endif;
-        else:
-            return false;
-        endif;
-    }
-    function maxPriceValue()
-    {
-        $maxPrice = DB::table('prices')->max('price');
-        return $maxPrice;
-    }
-    function maxLengthValue()
-    {
-        $maxPrice = DB::table('listings')->max('length');
-       return $maxPrice;
-    }
-    function minMaxPrice($season,$price='')
-    {
-        unset($season->id);
-        unset($season->listing_id);
-        unset($season->season_price_id);
-        unset($season->price);
-        unset($season->created_at);
-        unset($season->updated_at);
-        $season = $season ? $season->toArray() : [];
-        if($price)
-        {
-            $price = array($price);
-        }
-        if(is_array($season) && is_array($price))
-        {
-            $season = array_merge($season,$price);
-        }
-        $min = min($season);
-        $max = max($season);
-        if($min && $max)
-        {
-            return $min .' - '. $max;
-        }
-        else
-        {
-            if($min)
-            {
-                return $min ;
-            }
-            else
-            {
-                return $max ;
-            }
+            $result['status'] = 'error';
+            $result['message'] = 'Please select different date.';
         }
        
+        return $result;
     }
-?>
+}
+function selectOption($table, $columnName, $columnValue, $selectedValue = '', $condition = '', $orderBy = '')
+{
+    $html = "";
+
+    $items = DB::table($table)->select($columnValue, $columnName)
+        ->when(is_array($condition), function ($query) use ($condition) {
+            $query->where($condition[0], $condition[1]);
+        })
+        ->when(is_array($orderBy), function ($query) use ($condition) {
+            $query->orderBy($orderBy[0], $orderBy[1]);
+        })->get()->toArray();
+
+    if ($items):
+        foreach ($items as $item):
+            $item = (array) $item;
+            $selected = '';
+            if ($item[$columnValue] == $selectedValue):
+                $selected = 'selected';
+            endif;
+            $html .= '<option value = "' . $item[$columnValue] . '" ' . $selected . '>' . $item[$columnName] . '</option>';
+        endforeach;
+    endif;
+
+    return $html;
+}
+function checkCheckbox($array, $value)
+{
+    if (!empty($array)):
+        if (in_array($value, $array)):
+            return 'checked';
+        else:
+            return false;
+        endif;
+    else:
+        return false;
+    endif;
+}
+function singleCheckbox($selected, $value)
+{
+    if (!empty($selected)):
+        if ($value == $selected):
+            return 'checked';
+        else:
+            return false;
+        endif;
+    else:
+        return false;
+    endif;
+}
+function checkradio($selected, $value)
+{
+    if (!empty($selected)):
+        if ($value == $selected):
+            return 'checked';
+        else:
+            return false;
+        endif;
+    else:
+        return false;
+    endif;
+}
+function checkselect($selected, $value)
+{
+    if (!empty($selected)):
+        if ($value == $selected):
+            return 'selected';
+        else:
+            return false;
+        endif;
+    else:
+        return false;
+    endif;
+}
+function maxPriceValue()
+{
+    $maxPrice = DB::table('prices')->max('price');
+    return $maxPrice;
+}
+function maxLengthValue()
+{
+    $maxPrice = DB::table('listings')->max('length');
+    return $maxPrice;
+}
+function minMaxPrice($season, $price = '')
+{
+    unset($season->id);
+    unset($season->listing_id);
+    unset($season->season_price_id);
+    unset($season->price);
+    unset($season->created_at);
+    unset($season->updated_at);
+    $season = $season ? $season->toArray() : [];
+    if ($price) {
+        $price = array($price);
+    }
+    if (is_array($season) && is_array($price)) {
+        $season = array_merge($season, $price);
+    }
+    $min = min($season);
+    $max = max($season);
+    if ($min && $max) {
+        return $min . ' - ' . $max;
+    } else {
+        if ($min) {
+            return $min;
+        } else {
+            return $max;
+        }
+    }
+}
