7bf105aad5823003cb373e346f75bbdb6269614e
abhishekthakur98 <abhishek@so-creative.co.uk>
2025-04-29 15:25:54 +0530

    designchange29-04-2025

--- resources/views/front/single_boat.blade.php
+++ resources/views/front/single_boat.blade.php
@@ -895,6 +895,54 @@
     <div class="offcanvas-right" id="price-list">
         <span class="close-btn" id="closeMenu">&times;</span>
         <h3 class="p-4">Price list</h3>
+        <div class="price_list_accordian">
+        <div id="accordion">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+        <h5 class="mb-0">
+            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            Price
+            </button>
+        </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+        <div class="card-body">
+            <p>100</p>
+        </div>
+    </div>
+    </div>
+    <div class="card">
+        <div class="card-header" id="headingTwo">
+            <h5 class="mb-0">
+                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Low Season Price
+                </button>
+            </h5>
+        </div>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+            <div class="card-body">
+            <p>$100</p>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header" id="headingThree">
+            <h5 class="mb-0">
+                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+               Mid Season Price
+                </button>
+            </h5>
+        </div>
+        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+            <div class="card-body">
+            <p>$4-$555</p>
+            </div>
+        </div>
+    </div>
+    </div>
+
+        </div>
         <ul class="p-4 list-unstyled">
             @if (optional($listing->price ?? null)->price)
                 <li>
