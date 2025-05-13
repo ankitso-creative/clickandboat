63c0fe321d3380dbafe94d694fe32177346de371
abhishekthakur98 <abhishek@so-creative.co.uk>
2025-05-13 17:38:04 +0530

    designchang-13-05-2025

--- resources/views/layouts/boatowner/common.blade.php
+++ resources/views/layouts/boatowner/common.blade.php
@@ -28,6 +28,34 @@
     <!-- Loader-->
     <div id="page-preloader"><span class="spinner border-t_second_b border-t_prim_a"></span></div>
     <!-- Loader end-->
+          <ul class="nav-mobile custmer_menu" id="mobnavbar">
+        <li><a href="{{ route('home') }}"><img src="{{ logoURL() }}"></a></li>
+            <li class="menu-container">
+                <input id="menu-toggle" type="checkbox">
+                <label for="menu-toggle" class="menu-button">
+                    <svg class="icon-open" viewBox="0 0 24 24">
+                        <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+                    </svg>
+                    <svg class="icon-close" viewBox="0 0 100 100">
+                        <path
+                            d="M83.288 88.13c-2.114 2.112-5.575 2.112-7.69 0L53.66 66.188c-2.113-2.112-5.572-2.112-7.686 0l-21.72 21.72c-2.114 2.113-5.572 2.113-7.687 0l-4.693-4.692c-2.114-2.114-2.114-5.573 0-7.688l21.72-21.72c2.112-2.115 2.112-5.574 0-7.687L11.87 24.4c-2.114-2.113-2.114-5.57 0-7.686l4.842-4.842c2.113-2.114 5.57-2.114 7.686 0l21.72 21.72c2.114 2.113 5.572 2.113 7.688 0l21.72-21.72c2.115-2.114 5.574-2.114 7.688 0l4.695 4.695c2.112 2.113 2.112 5.57-.002 7.686l-21.72 21.72c-2.112 2.114-2.112 5.573 0 7.686L88.13 75.6c2.112 2.11 2.112 5.572 0 7.687l-4.842 4.84z" />
+                    </svg>
+                </label>
+                <ul class="menu-sidebar">
+                    @php 
+                        $unreadCount = App\Models\Message::where('receiver_id', auth()->id())->where('seen', 0)->count();
+                    @endphp
+                        <li class="{{ $active=='dashboard' ? 'active':'' }}"><a href="{{ route('boatowner.dashboard') }}"><i class="fas fa-th"></i> Dashboard</a></li>
+                        <li class="{{ $active=='profile' ? 'active':'' }}"><a href="{{ route('boatowner.profile') }}"><i class="fas fa-user-circle"></i> Profile</a></li>
+                        <li class="{{ $active=='listing' ? 'active':'' }}"><a href="{{ route('boatowner.listing') }}"><i class="fa-solid fa-list"></i> Listing</a></li>
+                        <li class="{{ $active=='customers' ? 'active':'' }}"><a href="{{ route('boatowner.customers') }}"><i class="fa-solid fa-users"></i> Customers</a></li>
+                        <li class="{{ $active=='booking' ? 'active':'' }}"><a href="{{ route('boatowner.booking.index') }}"><i class="fas fa-clipboard-list"></i> Bookings</a></li>
+                        <li class="{{ $active=='support' ? 'active':'' }}"><a href="{{ route('boatowner.support')}}"><i class="fas fa-heart"></i> Messages @if($unreadCount) <span>({{ $unreadCount }}) </span> @endif </a></li>
+                        <li><a href="{{ route('logout') }}"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+                      
+                </ul>
+            </li>
+        </ul>
     <div class="l-theme animated-css" data-header="sticky" data-header-top="200" data-canvas="container">
         <header class="p-3 header-dashboard">
             <div class="container-fluid">
@@ -70,7 +98,7 @@
             <div class="row account-overview">
                 @if($active !='listing')
                     <div class="col-lg-3">
-                        <aside class="sidebar">
+                        <aside class="sidebar desktop_sidebar">
                             <div class="user">
                             <div class="user-avatar">
                                 <a href="#">
@@ -166,6 +194,20 @@
     {{-- <script src="{{ asset('app-assets/site_assets/js/map.js') }}"></script> --}}
     <script src="{{ asset('app-assets/site_assets/js/slick.min.js') }}"></script>
     <script src="{{ asset('app-assets/site_assets/js/script.js') }}?ver=<?=time()?>"></script>
+     <script>
+        // Replace '#navbar' with your actual navbar selector
+const navbar = document.querySelector('#mobnavbar');
+
+// Scroll event
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+    </script>
 
     @yield('js')
 
